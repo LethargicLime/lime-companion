c2cfb117b310932ae9540fab2f5acd38bb01b3f1
LethargicLime <majesticallime@gmail.com>
2023-10-20 18:02:47 -0700

    attempts to make live

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: "export",
+  basePath: "/nextjs-github-pages",
+  images: {
+    unoptimized: true
+  }
 }
 
 module.exports = nextConfig
