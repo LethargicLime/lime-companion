7729eb3c5abd7614595a19315c8969ef885601f7
LethargicLime <majesticallime@gmail.com>
2023-10-20 18:44:26 -0700

    modified:   next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  basePath: "/nextjs-github-pages",
+  output: "export",
+  basePath: "/lime-companion",
   images: {
     unoptimized: true
   }
