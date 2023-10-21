2459ece317a1d7133c46817d9ab63ad39aae540f
LethargicLime <majesticallime@gmail.com>
2023-10-20 17:48:07 -0700

    Updates

---
+++ static/983PAcjVCcDzWszF72Nc5/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/css/e7d3bab42d9af29d.css","static/chunks/pages/index-522d760804953061.js"],"/_error":["static/chunks/pages/_error-08a9db0f433628d8.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
