19f13c96523eb971866a9860a1bd226108da3604
LethargicLime <majesticallime@gmail.com>
2025-04-15 21:30:26 -0400

    Update home.tsx

--- pages/api/home.tsx
+++ pages/api/home.tsx
@@ -26,6 +26,8 @@ export default function Page() {
             tempKeys.includes(key) || tempKeys.push(key);
         }
 
+        console.log(imageObject);
+
         updateKeys(tempKeys);
     }, []);
 
